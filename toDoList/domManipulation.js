4996ce2effb5ccf67ff6bb8247f05eebc9a631ac
4444Fede <federico.vega@gm2dev.com>
2024-08-23 12:52:09 -0300

    feat(toDoList): add something

--- toDoList/domManipulation.js
+++ toDoList/domManipulation.js
@@ -2,10 +2,10 @@
 Extra: alert users to see if they are sure
 Add a button to toggle light/dark mode.
 */
-let toggleButton = document.querySelector('.mode-button')
+let toggleButton = document.querySelector(".mode-button");
 let lists = [
   ["Buy eggs", "Do laundry", "Buy facturas for Seba"],
-  ["Sleep", "Eat", "Code", "Repeat"]
+  ["Sleep", "Eat", "Code", "Repeat"],
 ];
 
 function renderTodoList(listId, list) {
@@ -74,28 +74,30 @@ function createDeleteButton(list) {
     let listItem = this.parentNode;
     let listElement = listItem.parentNode;
     let listIndex = 0;
-    for (let i = 0; i < listElement.children.length; i++) {
-      if (listElement.children[i] === listItem) {
-        listIndex = i;
-        break;
+    if (confirm("Estas seguro de querer borrarlo?")) {
+      for (let i = 0; i < listElement.children.length; i++) {
+        if (listElement.children[i] === listItem) {
+          listIndex = i;
+          break;
+        }
       }
+      let listId = listElement.id;
+      let listIdNumber = parseInt(listId.substring(5), 10) - 1;
+      let updatedList = list.filter(function (_, index) {
+        return index !== listIndex;
+      });
+      lists[listIdNumber] = updatedList;
+      renderTodoList(listId, updatedList);
     }
-    let listId = listElement.id;
-    let listIdNumber = parseInt(listId.substring(5), 10) - 1;
-    let updatedList = list.filter(function (_, index) {
-      return index !== listIndex;
-    });
-    lists[listIdNumber] = updatedList;
-    renderTodoList(listId, updatedList);
   });
   return deleteButton;
 }
-toggleButton.addEventListener('click', function(){
-    let btnIcon = document.querySelector('i')
-    document.querySelector('body').classList.toggle('dark-body')
-    btnIcon.classList.toggle('fa-moon')
-    btnIcon.classList.toggle('fa-sun')
-})
+toggleButton.addEventListener("click", function () {
+  let btnIcon = document.querySelector("i");
+  document.querySelector("body").classList.toggle("dark-body");
+  btnIcon.classList.toggle("fa-moon");
+  btnIcon.classList.toggle("fa-sun");
+});
 
 window.addEventListener("load", function () {
   for (let i = 0; i < lists.length; i++) {
